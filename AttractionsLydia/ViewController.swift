ff9c1d86d803f88de6d7674e5bfdf94c9d1a2419
Lydia-Lu-code <lydialu8822@gmail.com>
2025-07-27 16:05:58 +0800

    Initial Commit

---
+++ AttractionsLydia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AttractionsLydia
+//
+//  Created by Lydia Lu on 2025/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
